578b47b93fd61f33dbf13ffa8435af5824e3ff80
Abdullah Ameen <abdullahamen321@gmail.com>
2026-08-21 00:10:04 +0500

    Time: 730 ms (73.61%), Space: 18.2 MB (81.15%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.py
+++ LeetCode/Medium/0015-3sum/0015-3sum.py
@@ -4,15 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ans = set()
-        m = {}
+        nums.sort()
+        ans = []
         n = len(nums)
         for i in range(n):
-            hashset = set()
-            for j in range(i+1,n):
-                third = -(nums[i]+nums[j])
-                if third in hashset:
-                    triplet = tuple(sorted([nums[i], nums[j], third]))
-                    ans.add(triplet)
-                hashset.add(nums[j])
-        return [list(t) for t in ans]
+            if i > 0 and nums[i] == nums[i-1]:continue
+            j = i + 1
+            k = n - 1
+            while j < k:
+                tar = nums[i] + nums[j] + nums[k]
+                if tar < 0:
+                    j+=1
+                elif tar > 0:
+                    k-=1
+                else:
+                    temp = [nums[i],nums[j],nums[k]]
+                    ans.append(temp)
+                    j+=1
+                    k-=1
+                    while(j < k and nums[j] == nums[j-1]):j+=1
+                    while(j < k and nums[k] == nums[k+1]):k-=1
+
+            
+        return ans

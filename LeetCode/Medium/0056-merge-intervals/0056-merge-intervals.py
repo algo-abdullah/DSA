4d12127099afb22f221264e1536c7104bc1450c1
Abdullah Ameen <abdullahamen321@gmail.com>
2026-08-22 23:42:10 +0500

    Time: 11 ms (56.4%), Space: 17 MB (26.64%) - LeetHub

---
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort()
+        ans = []
+        n = len(intervals)
+        i = 0
+        while i < n:
+            start = intervals[i][0]
+            end = intervals[i][1]
+            j = i + 1
+            while j < n and intervals[j][0] <= end:
+                end = max(intervals[j][1], end)
+                j = j + 1
+
+            ans.append([start,end])
+            i = j
+        return ans
+        

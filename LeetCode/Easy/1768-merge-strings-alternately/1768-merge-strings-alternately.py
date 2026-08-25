d83ca978cbad6e920be51412e502e06d3e11116c
Abdullah Ameen <abdullahamen321@gmail.com>
2026-08-25 23:14:56 +0500

    Time: 19 ms (35.59%), Space: 12.4 MB (21.22%) - LeetHub

---
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        merged = ""
+        i = 0
+        j = 0
+        while i < len(word1) and j < len(word2):
+           
+            merged+=word1[i]
+            i+=1
+            merged+=word2[j]
+            j+=1
+        while i < len(word1):
+            merged+=word1[i]
+            i+=1
+        while j < len(word2):
+            merged+=word2[j]
+            j+=1
+        return merged
+        

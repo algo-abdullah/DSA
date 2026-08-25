70222443f46cedc0f3fe425709b7052f7c47a7fa
Abdullah Ameen <abdullahamen321@gmail.com>
2026-08-25 23:42:56 +0500

    Time: 7 ms (27.66%), Space: 12.5 MB (3.17%) - LeetHub

---
+++ LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        m = {}
+        for i  in range(len(t)):
+            
+            m[t[i]] = m.get(t[i] , 0) +1
+        for i in range(len(s)):
+            m[s[i]] -= 1
+            if m[s[i]] == 0:
+                del m[s[i]]
+        return list(m.keys())[0]
+        

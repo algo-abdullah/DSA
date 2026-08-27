2101434cf794478de15a8cd50a7a7c6183f6ec6c
Abdullah Ameen <abdullahamen321@gmail.com>
2026-08-27 23:05:18 +0500

    Time: 0 ms (100%), Space: 12.8 MB (66.24%) - LeetHub

--- LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.py
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.py
@@ -5,22 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-    
         start=0
         end=len(nums)-1
-     
         while(start<=end):
-
             mid = start+(end-start)//2
             if(target==nums[mid]):
                 return mid
             elif(nums[mid]<target):
-            
                 start =mid+1
             else:
-            
-                end=mid-1
-                
+                end=mid-1       
             
         
         return start

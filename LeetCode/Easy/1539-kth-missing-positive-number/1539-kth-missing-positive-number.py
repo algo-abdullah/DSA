69ef8bf91c5cbeddc963b96600397a89b3d9f179
Abdullah Ameen <abdullahamen321@gmail.com>
2026-09-13 18:23:25 +0500

    Time: 4 ms (27.83%), Space: 12.5 MB (18.09%) - LeetHub

--- LeetCode/Easy/1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ LeetCode/Easy/1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -5,9 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        for i in range(len(arr)):
-            if arr[i]<= k:
-                k+=1
-            else:break
-        return k
+        low = 0 
+        high = len(arr)- 1
+        while low <= high:
+            mid = low +(high - low)//2
+            missing = arr[mid] - (mid +1)
+            if missing < k:
+                low = mid +1
+            else:
+                high = mid - 1
+            
+        return low+k
         

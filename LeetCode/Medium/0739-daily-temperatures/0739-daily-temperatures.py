f29601c9731f58da049a6cafde1c37f4750e75b4
Abdullah Ameen <abdullahamen321@gmail.com>
2026-09-26 23:40:50 +0500

    Time: 148 ms (88.96%), Space: 24.4 MB (91.69%) - LeetHub

---
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        res = [0]* len(temperatures)
+        stack = []
+        for i , t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                idx = stack.pop()
+                res[idx] = i  - idx
+            stack.append(i)
+        return res
+        
